5d2d3d38e9586e4444efe5785b36197448579e66
Ezekiel Clark <clarkzeke@gmail.com>
2019-12-04 14:28:26 -0700

    Ice cream paint job

--- lib/vehicle_analysis.rb
+++ lib/vehicle_analysis.rb
@@ -3,31 +3,14 @@
 class VehicleAnalysis
 
   def analyze(vehicle)
-    if vehicle.car?
-      if vehicle.four_wheel_drive? || !vehicle.four_wheel_drive?
-        puts "Vehicle has four wheels "
-        if vehicle.four_wheel_drive?
-          puts "with four wheel drive"
-        else
-          puts "with two wheel drive"
-        end
-      end
-    elsif vehicle.tractor?
-      puts "Vehicle has four wheels "
-      if vehicle.big_back_wheels?
-        puts "with big wheels in the back"
-      end
-    elsif vehicle.pickup?
-      puts "Vehicle has four wheels "
-      if vehicle.four_wheel_drive?
-        puts "with four wheel drive"
-      else
-        puts "with two wheel drive"
-      end
-      if vehicle.big_back_wheels?
-        puts "with big wheels in the back"
-      end
+    puts "Vehicle has four wheels"
+    if vehicle.four_wheel_drive?
+      puts "with four wheel drive"
+    else
+      puts "with two wheel drive"
+    end
+    if vehicle.big_back_wheels?
+      puts "with big wheels in the back"
     end
   end
-
 end
